000c934a4eced8a9cc5dc8dc8a7865898f565b23
roundoutandabout <rockingcat@tutanota.com>
2023-02-13 21:30:09 +0100

    Add comments

--- keyboards/gmmk/pro/rev1/ansi/keymaps/woody/rules.mk
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/woody/rules.mk
@@ -1,2 +1,2 @@
-LTO_ENABLE = yes               # link time optimization -- achieves a smaller compiled size
-VIA_ENABLE = yes 
+LTO_ENABLE = yes	# link time optimization -- achieves a smaller compiled size
+VIA_ENABLE = yes	# VIA support enabled 
